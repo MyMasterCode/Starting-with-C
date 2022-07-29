fcd6fad6de50c2b76d871127ad50cd60d12b89af
MyMasterCode <105924663+MyMasterCode@users.noreply.github.com>
2022-07-29 18:15:41 +0000

    Code Climate badge

---
+++ scr/app/components/roadmap/util/data-bridge-service-subscriber.ts
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  
+  
+  
+  
+  
+  
+  <head>
+    <meta id="bb-bootstrap" data-current-user="{&quot;username&quot;: &quot;OSali&quot;, &quot;isTeam&quot;: false, &quot;avatarUrl2x&quot;: &quot;https://bitbucket.org/account/OSali/avatar/64/?ts=1527091613&quot;, &quot;id&quot;: 9001705, &quot;displayName&quo
